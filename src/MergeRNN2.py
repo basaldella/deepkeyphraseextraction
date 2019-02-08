8c24cb2596e3035490c576052c92067f472e7967
peppe69 <giuslan@gmail.com>
2019-02-08 12:30:40 +0100

    Funzionante su Hulth2003 - i glove embeddings sono sotto data/glove; - i trained models sono salvati/caricati in models; - cambiati i path relativi;

--- src/MergeRNN2.py
+++ src/MergeRNN2.py
@@ -46,18 +46,18 @@ info.log_versions()
 # GLOBAL VARIABLES
 
 SAVE_MODEL = True
-MODEL_PATH = "models/mergernn2.h5"
+MODEL_PATH = "../models/mergernn2.h5"
 SHOW_PLOTS = True
 
 # END GLOBAL VARIABLES
 
 # Dataset and hyperparameters for each dataset
 
-DATASET = Semeval2017
+DATASET = Hulth
 
 if DATASET == Semeval2017:
     tokenizer = tk.tokenizers.nltk
-    DATASET_FOLDER = "data/Semeval2017"
+    DATASET_FOLDER = "../data/Semeval2017"
     MAX_DOCUMENT_LENGTH = 550
     MAX_VOCABULARY_SIZE = 20000
     EMBEDDINGS_SIZE = 300
@@ -65,7 +65,7 @@ if DATASET == Semeval2017:
     EPOCHS = 36
 elif DATASET == Hulth:
     tokenizer = tk.tokenizers.nltk
-    DATASET_FOLDER = "data/Hulth2003"
+    DATASET_FOLDER = "../data/Hulth2003"
     MAX_DOCUMENT_LENGTH = 550
     MAX_VOCABULARY_SIZE = 20000
     EMBEDDINGS_SIZE = 300
@@ -73,7 +73,7 @@ elif DATASET == Hulth:
     EPOCHS = 43
 elif DATASET == Marujo2012:
     tokenizer = tk.tokenizers.nltk
-    DATASET_FOLDER = "data/Marujo2012"
+    DATASET_FOLDER = "../data/Marujo2012"
     MAX_DOCUMENT_LENGTH = 8000
     MAX_VOCABULARY_SIZE = 20000
     EMBEDDINGS_SIZE = 300
@@ -81,7 +81,7 @@ elif DATASET == Marujo2012:
     EPOCHS = 13
 elif DATASET == Kp20k:
     tokenizer = tk.tokenizers.nltk
-    DATASET_FOLDER = "data/Kp20k"
+    DATASET_FOLDER = "../data/Kp20k"
     MAX_DOCUMENT_LENGTH = 1912  # gl: was 540
     MAX_VOCABULARY_SIZE = 400000
     EMBEDDINGS_SIZE = 300
@@ -101,10 +101,10 @@ train_doc_str, train_answer_str = data.load_train()
 test_doc_str, test_answer_str = data.load_test()
 val_doc_str, val_answer_str = data.load_validation()
 
-train_doc, train_answer = tk.tokenize_set(train_doc_str,train_answer_str,tokenizer)
-test_doc, test_answer = tk.tokenize_set(test_doc_str,test_answer_str,tokenizer)
+train_doc, train_answer = tk.tokenize_set(train_doc_str, train_answer_str, tokenizer)
+test_doc, test_answer = tk.tokenize_set(test_doc_str, test_answer_str, tokenizer)
 if val_doc_str and val_answer_str:
-    val_doc, val_answer = tk.tokenize_set(val_doc_str,val_answer_str,tokenizer)
+    val_doc, val_answer = tk.tokenize_set(val_doc_str, val_answer_str, tokenizer)
 else:
     val_doc = None
     val_answer = None
@@ -114,17 +114,19 @@ else:
 
 logging.info("Dataset loaded. Preprocessing data...")
 
-train_x,train_y,test_x,test_y,val_x,val_y,embedding_matrix = preprocessing.\
-    prepare_sequential(train_doc, train_answer, test_doc, test_answer,val_doc,val_answer,
+train_x, train_y, test_x, test_y, val_x, val_y, embedding_matrix = preprocessing.\
+    prepare_sequential(train_doc, train_answer, test_doc, test_answer, val_doc, val_answer,
                        max_document_length=MAX_DOCUMENT_LENGTH,
                        max_vocabulary_size=MAX_VOCABULARY_SIZE,
                        embeddings_size=EMBEDDINGS_SIZE)
 
+'''
 # weigh training examples: everything that's not class 0 (not kp)
 # gets a heavier score
 #train_y_weights = np.argmax(train_y,axis=2) # this removes the one-hot representation
 #train_y_weights[train_y_weights > 0] = 20
 #train_y_weights[train_y_weights < 1] = 1
+'''
 
 from sklearn.utils import class_weight
 train_y_weights = np.argmax(train_y, axis=2)
@@ -134,20 +136,20 @@ train_y_weights = np.reshape(class_weight.compute_sample_weight('balanced', trai
 logging.info("Data preprocessing complete.")
 logging.info("Maximum possible recall: %s",
              metrics.recall(test_answer,
-                               postprocessing.get_words(test_doc,postprocessing.undo_sequential(test_y))))
+                            postprocessing.get_words(test_doc, postprocessing.undo_sequential(test_y))))
 
-if not SAVE_MODEL or not os.path.isfile(MODEL_PATH) :
+if not SAVE_MODEL or not os.path.isfile(MODEL_PATH):
 
     logging.debug("Building the network...")
 
     summary = layers.Input(shape=(MAX_DOCUMENT_LENGTH,))
-    #print(summary)  # gl
+    # print(summary)  # gl
     encoded_summary = layers.Embedding(np.shape(embedding_matrix)[0],
-                        EMBEDDINGS_SIZE,
-                        weights=[embedding_matrix],
-                        input_length=MAX_DOCUMENT_LENGTH,
-                        trainable=False)(summary)
-    #print(encoded_summary)  # gl
+                                       EMBEDDINGS_SIZE,
+                                       weights=[embedding_matrix],
+                                       input_length=MAX_DOCUMENT_LENGTH,
+                                       trainable=False)(summary)
+    # print(encoded_summary)  # gl
     encoded_summary = layers.Conv1D(filters=128, kernel_size=32, strides=4, activation='relu')(encoded_summary)
     # Size: 131
     encoded_summary = layers.MaxPool1D(pool_size=2)(encoded_summary)
@@ -168,18 +170,18 @@ if not SAVE_MODEL or not os.path.isfile(MODEL_PATH) :
 
     document = layers.Input(shape=(MAX_DOCUMENT_LENGTH,))
     encoded_document = layers.Embedding(np.shape(embedding_matrix)[0],
-                        EMBEDDINGS_SIZE,
-                        weights=[embedding_matrix],
-                        input_length=MAX_DOCUMENT_LENGTH,
-                        trainable=False)(document)
+                                        EMBEDDINGS_SIZE,
+                                        weights=[embedding_matrix],
+                                        input_length=MAX_DOCUMENT_LENGTH,
+                                        trainable=False)(document)
 
     print(np.shape(encoded_summary))  # gl: intermed. values
     print(np.shape(encoded_document))  # gl: intermed. values
 
     merged = layers.add([encoded_summary, encoded_document])
-    merged = layers.Bidirectional(layers.LSTM((int)(EMBEDDINGS_SIZE/2), return_sequences=True))(merged)
+    merged = layers.Bidirectional(layers.LSTM(int(EMBEDDINGS_SIZE/2), return_sequences=True))(merged)
     merged = layers.Dropout(0.3)(merged)
-    merged = layers.Bidirectional(layers.LSTM((int)(EMBEDDINGS_SIZE/4), return_sequences=True))(merged)
+    merged = layers.Bidirectional(layers.LSTM(int(EMBEDDINGS_SIZE/4), return_sequences=True))(merged)
     merged = layers.Dropout(0.3)(merged)
     prediction = layers.TimeDistributed(layers.Dense(3, activation='softmax'))(merged)
 
@@ -190,41 +192,41 @@ if not SAVE_MODEL or not os.path.isfile(MODEL_PATH) :
                   sample_weight_mode="temporal")
     print(model.summary())
 
-    metrics_callback = keras_metrics.MetricsCallback([val_x,val_x],val_y)
+    metrics_callback = keras_metrics.MetricsCallback([val_x, val_x], val_y)
 
     logging.info("Fitting the network...")
-    history = model.fit([train_x,train_x], train_y,
-                        validation_data=([val_x,val_x],val_y),
+    history = model.fit([train_x, train_x], train_y,
+                        validation_data=([val_x, val_x], val_y),
                         epochs=EPOCHS,
                         batch_size=BATCH_SIZE,
                         sample_weight=train_y_weights,
                         callbacks=[metrics_callback])
 
-    if SHOW_PLOTS :
+    if SHOW_PLOTS:
         plots.plot_accuracy(history)
         plots.plot_loss(history)
         plots.plot_prf(metrics_callback)
 
-    if SAVE_MODEL :
+    if SAVE_MODEL:
         model.save(MODEL_PATH)
         logging.info("Model saved in %s", MODEL_PATH)
 
-else :
-    logging.info("Loading existing model from %s...",MODEL_PATH)
+else:
+    logging.info("Loading existing model from %s...", MODEL_PATH)
     model = load_model(MODEL_PATH)
     logging.info("Completed loading model from file")
 
 
 logging.info("Predicting on test set...")
-output = model.predict(x=[test_x,test_x], verbose=1)
-logging.debug("Shape of output array: %s",np.shape(output))
+output = model.predict(x=[test_x, test_x], verbose=1)
+logging.debug("Shape of output array: %s", np.shape(output))
 
 obtained_tokens = postprocessing.undo_sequential(output)
-obtained_words = postprocessing.get_words(test_doc,obtained_tokens)
+obtained_words = postprocessing.get_words(test_doc, obtained_tokens)
 
-precision = metrics.precision(test_answer,obtained_words)
-recall = metrics.recall(test_answer,obtained_words)
-f1 = metrics.f1(precision,recall)
+precision = metrics.precision(test_answer, obtained_words)
+recall = metrics.recall(test_answer, obtained_words)
+f1 = metrics.f1(precision, recall)
 
 print("###    Obtained Scores    ###")
 print("###     (full dataset)    ###")
@@ -234,9 +236,9 @@ print("### Recall    : %.4f" % recall)
 print("### F1        : %.4f" % f1)
 print("###                       ###")
 
-keras_precision = keras_metrics.keras_precision(test_y,output)
-keras_recall = keras_metrics.keras_recall(test_y,output)
-keras_f1 = keras_metrics.keras_f1(test_y,output)
+keras_precision = keras_metrics.keras_precision(test_y, output)
+keras_recall = keras_metrics.keras_recall(test_y, output)
+keras_f1 = keras_metrics.keras_f1(test_y, output)
 
 print("###    Obtained Scores    ###")
 print("###    (fixed dataset)    ###")
@@ -248,9 +250,9 @@ print("###                       ###")
 
 clean_words = postprocessing.clean_answers(obtained_words)
 
-precision = metrics.precision(test_answer,clean_words)
-recall = metrics.recall(test_answer,clean_words)
-f1 = metrics.f1(precision,recall)
+precision = metrics.precision(test_answer, clean_words)
+recall = metrics.recall(test_answer, clean_words)
+f1 = metrics.f1(precision, recall)
 
 print("###    Obtained Scores    ###")
 print("### (full dataset,        ###")
@@ -312,8 +314,8 @@ print("###                       ###")
 
 STEM_MODE = metrics.stemMode.both
 
-precision = metrics.precision(test_answer, obtained_words,STEM_MODE)
-recall = metrics.recall(test_answer, obtained_words,STEM_MODE)
+precision = metrics.precision(test_answer, obtained_words, STEM_MODE)
+recall = metrics.recall(test_answer, obtained_words, STEM_MODE)
 f1 = metrics.f1(precision, recall)
 
 print("###    Obtained Scores    ###")
@@ -338,8 +340,8 @@ print("###                       ###")
 
 clean_words = postprocessing.get_valid_patterns(obtained_words)
 
-precision = metrics.precision(test_answer, clean_words,STEM_MODE)
-recall = metrics.recall(test_answer, clean_words,STEM_MODE)
+precision = metrics.precision(test_answer, clean_words, STEM_MODE)
+recall = metrics.recall(test_answer, clean_words, STEM_MODE)
 f1 = metrics.f1(precision, recall)
 
 print("###    Obtained Scores    ###")
@@ -353,8 +355,8 @@ print("###                       ###")
 
 obtained_words_top = postprocessing.get_top_words(test_doc, output, 5)
 
-precision_top = metrics.precision(test_answer, obtained_words_top,STEM_MODE)
-recall_top = metrics.recall(test_answer, obtained_words_top,STEM_MODE)
+precision_top = metrics.precision(test_answer, obtained_words_top, STEM_MODE)
+recall_top = metrics.recall(test_answer, obtained_words_top, STEM_MODE)
 f1_top = metrics.f1(precision_top, recall_top)
 
 print("###    Obtained Scores    ###")
@@ -367,8 +369,8 @@ print("###                       ###")
 
 obtained_words_top = postprocessing.get_top_words(test_doc, output, 10)
 
-precision_top = metrics.precision(test_answer, obtained_words_top,STEM_MODE)
-recall_top = metrics.recall(test_answer, obtained_words_top,STEM_MODE)
+precision_top = metrics.precision(test_answer, obtained_words_top, STEM_MODE)
+recall_top = metrics.recall(test_answer, obtained_words_top, STEM_MODE)
 f1_top = metrics.f1(precision_top, recall_top)
 
 print("###    Obtained Scores    ###")
@@ -381,8 +383,8 @@ print("###                       ###")
 
 obtained_words_top = postprocessing.get_top_words(test_doc, output, 15)
 
-precision_top = metrics.precision(test_answer, obtained_words_top,STEM_MODE)
-recall_top = metrics.recall(test_answer, obtained_words_top,STEM_MODE)
+precision_top = metrics.precision(test_answer, obtained_words_top, STEM_MODE)
+recall_top = metrics.recall(test_answer, obtained_words_top, STEM_MODE)
 f1_top = metrics.f1(precision_top, recall_top)
 
 print("###    Obtained Scores    ###")
@@ -395,6 +397,6 @@ print("###                       ###")
 
 if DATASET == Semeval2017:
     from eval import anno_generator
-    anno_generator.write_anno("/tmp/mergernn2",test_doc_str,clean_words)
+    anno_generator.write_anno("/tmp/mergernn2", test_doc_str, clean_words)
     from data.Semeval2017 import eval
-    eval.calculateMeasures("data/Semeval2017/test","/tmp/simplernn",remove_anno=["types"])
+    eval.calculateMeasures("data/Semeval2017/test", "/tmp/simplernn", remove_anno=["types"])
