5e3c90a981e1a715b1c3d852120a5414a79c1452
Marco Basaldella <basaldella.marco@gmail.com>
2019-02-19 12:49:40 +0000

    Docker improvements: parametrized username, better directory

--- start_container.sh
+++ start_container.sh
@@ -2,15 +2,22 @@
 
 source variables.sh
 
+mkdir -p /tmp/$IMAGE_NAME
+
+WORKDIR="/home/$DOCKER_USER"
+VOLUMES="-v $(pwd)/src:${WORKDIR}/src -v $(pwd)/data:${WORKDIR}/data -v $(pwd)/models:${WORKDIR}/models -v /tmp/${IMAGE_NAME}/:${WORKDIR}/tmp"
+
+echo ${VOLUMES}
+
 if [ $# -eq 0 ]; then
 
-    ${DOCKER_BIN} run -it --rm -v "$(pwd)/src":/opt/proj/src -v "$(pwd)/data":/opt/proj/data -v "$(pwd)/models":/opt/proj/models --user $(id -u):$(id -g) --name ${CONTAINER_NAME} ${IMAGE_NAME}
+    ${DOCKER_BIN} run -it --rm --user $(id -u):$(id -g) ${VOLUMES} --name ${CONTAINER_NAME} ${IMAGE_NAME}
     exit 0
 fi
 
 if [ $# -eq 1 ]; then
     if [ "$1" == "-bg" ]; then
-        ${DOCKER_BIN} run -d -v "$(pwd)/src":/opt/proj/src -v "$(pwd)/data":/opt/proj/data -v "$(pwd)/models":/opt/proj/models --user $(id -u):$(id -g) --name ${CONTAINER_NAME} ${IMAGE_NAME}
+        ${DOCKER_BIN} run -d ${VOLUMES} --user $(id -u):$(id -g) --name ${CONTAINER_NAME} ${IMAGE_NAME}
         exit 0
     fi
 fi
--- variables.sh
+++ variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DOCKER_BIN=nvidia-docker
+DOCKER_USER=docker-user
 IMAGE_NAME=dke
 CONTAINER_NAME=$IMAGE_NAME-container
+
 
